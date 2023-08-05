76d57b01148b2a3b3e07d4294d777c7ae362fec1
Joffrey Bienvenu <joffreybvn@gmail.com>
2023-08-05 13:09:55 +0200

    feat: Move default nodes and edges to external files

--- src/FlowSection.jsx
+++ src/FlowSection.jsx
@@ -9,17 +9,12 @@ import ReactFlow, {
 } from 'reactflow';
 
 import 'reactflow/dist/style.css';
-
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
-
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+import defaultNodes from './graph/nodes.js';
+import defaultEdges from './graph/edges.js';
 
 function Flow() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [nodes, setNodes, onNodesChange] = useNodesState(defaultNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(defaultEdges);
 
   const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
 
